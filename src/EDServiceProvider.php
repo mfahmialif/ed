c644e1fc75a25c655d250057e03f93b10a65f8ab
mfahmialif <mfahmialif@gmail.com>
2022-11-12 11:15:39 +0700

    upload

---
+++ src/EDServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Mfahmialif\ED;
+
+use Illuminate\Support\ServiceProvider;
+
+class EDServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/htaccess' => base_path('/')
+        ], 'config');
+    }
+
+    public function register()
+    {
+    }
+}
